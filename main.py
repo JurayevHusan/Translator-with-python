ab5e013597402617de96d4a03c626fb8103a014d
Jurayev Husan <jurayevkhusan@mail.ru>
2024-10-14 18:15:18 +0500

    added translator

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-name = input("Enter your name: ")
-print("So your name is :",name)
+from googletrans import Translator
+
+tr= Translator()
+text = input("Enter Your Text : ")
+lg = input("Enter Your Language : ")
+a = tr.translate(text , dest=lg)
+print(lg,"the language : ",a.text)
